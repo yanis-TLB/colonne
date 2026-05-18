c90c5f8938a61146f80d4f6d031728a72418f9e5
yanis-TLB <touilebyanisse07@gmail.com>
2026-05-18 22:16:27 +0100

    "Ajout application HPLC"

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Gestion Colonnes HPLC - Labo Q")
+st.write("Bienvenue au Laboratoire Q !")
+
+if st.button("Cliquez-moi"):
+    st.balloons()
+    st.success("🎉 Ça fonctionne !")
